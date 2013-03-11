97dbb4be921d87825199de052d6358c58189b5f0
YumeAsakura <yume.sqi@gmail.com>
2013-03-11 00:41:45 -0400

    coin3

--- HR/coins.cpp
+++ HR/coins.cpp
@@ -53,11 +53,6 @@ int main(){
 		scanf("%c", &ch);
 	}
 
-	for(int row=0; row<n; row++){
-		for(int col=0; col<m; col++)
-			printf("%d ", b[row][col]);
-		printf("\n");
-	}
 
 	// if the target is too far away then just return -1
 	if(targetX + targetY > k){
@@ -66,8 +61,8 @@ int main(){
 	}
 
 	// store the four directions
-	int dx[] = {0, 0, 1, 0, -1};
-	int dy[] = {0, 1, 0, -1, 0};
+	int dx[] = {0, -1, 0, 1, 0};
+	int dy[] = {0, 0, 1, 0, -1};
 
 	// originally the label is -1(unvisited) and time is n*m (larger than the largest possibility)
 	int time[n][m], label[n][m]; 
@@ -97,6 +92,24 @@ int main(){
 
 		stQ.pop();
 		free(pSt);
+		
+		//printf("st: (%d, %d) t=%d l=%d \n", curX, curY, curTime, curLabel);
+		if(b[curX][curY] == 0){
+			isReached = true;
+			// free the allocated spaces
+			while(!stQ.empty()){
+				pSt = stQ.front();
+				stQ.pop();
+				free(pSt);
+			}
+			while(!pathQ.empty()){
+				pSt = pathQ.front();
+				pathQ.pop();
+				free(pSt);
+			}
+			printf("%d\n", curLabel);
+			return 0;
+		}
 
 		// if the time from this point is larger than the current start point then we can start from here
 		if(curTime < time[curX][curY]){
@@ -104,13 +117,12 @@ int main(){
 
 			// proceed until we cannot make a move
 			while(hasPath){
-				// record the current position into the queue
-				pSt = (point*)malloc(sizeof(point));
-				setPoint(pSt, curX, curY, curLabel, curTime);
-				pathQ.push(pSt);
-
 				time[curX][curY] = curTime;
 				label[curX][curY] = curLabel;
+
+				//printf("(%d, %d) t=%d l=%d \n", curX, curY, curTime, curLabel);
+				if(curTime == k)
+					break;
 
 				// check the next move
 				nextX = curX + dx[b[curX][curY]];
@@ -147,18 +159,20 @@ int main(){
 				// if time limit is not reached, put the current point into pathQ, and proceed to next
 				if(curTime < k){
 					pSt = (point*)malloc(sizeof(point));
-					setPoint(pSt, curX, curY, curTime, curLabel);
+					setPoint(pSt, curX, curY, curLabel, curTime);
 					pathQ.push(pSt);
 					curX = nextX;
 					curY = nextY;
 					curTime ++;
 				}
-				// else we have to end the current search since time limit is reached
-				else{
-					hasPath = false;
-				}
 			}
 
+			/*
+			for(int row=0; row<n; row++){
+				for(int col=0; col<m; col++)
+					printf("%d ", time[row][col]);
+				printf("\n");
+			}*/
 			// by the end of the current search, we need to:
 			// 1. clearn the current path
 			// 2. add new starting point from the neighbors of the current path
@@ -171,17 +185,19 @@ int main(){
 				curTime = pSt->time;
 				curLabel = pSt->label;
 
+				//printf("poped: (%d, %d) t=%d l=%d \n", curX, curY, curTime, curLabel);
 				// search for the four neighbors of an point in the path to check whether it could be set as the new starting point
 				for(int i=1; i<=4; i++){
 					nextX = curX + dx[i];
-					nextY = curX + dy[i];
+					nextY = curY + dy[i];
 
 					// if the neighbor is in bound
 					if(nextX>=0 && nextX<n && nextY>=0 && nextY<m){
 						// if the time in the neighbor is larger than curTime+1 then it could be regarded as a new starting point.
 						if(time[nextX][nextY] > curTime + 1){
 							pSt = (point*)malloc(sizeof(point));
-							setPoint(pSt, nextX, nextY, curTime+1, curLabel+1);
+							//printf("(%d, %d)eq: (%d, %d) t=%d l=%d \n", curX, curY, nextX, nextY, curTime+1, curLabel+1);
+							setPoint(pSt, nextX, nextY, curLabel+1, curTime+1);
 							stQ.push(pSt);
 						}
 					}
