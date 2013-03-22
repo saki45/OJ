cfea33203139e2fa4c8fafe4e2ed5e121e81af45
yume <yume12345@gmail.com>
2013-03-21 20:46:19 -0400

    3212lab

--- HR/lucky2.cpp
+++ HR/lucky2.cpp
@@ -27,32 +27,82 @@ void getPrimeMap(int *primeMap, int N){
 	}
 }
 
-void computeG(int ***g, int **st, int **ed){
+void computeG(long long ***g, long long ***h,  int **st, int **ed){
 	for(int i=0; i<19; i++){
 		for(int j=0; j<9*i+1; j++){
 			for(int k=0; k<81*i+1; k++){
 				g[i][j][k] = 0;
+				h[18-i][j][k] = 0;
 			}
 		}
 	}
 	for(int i=0; i<19; i++){
 		for(int j=0; j<9*i+1; j++){
-			st[i][j] = 9*i+1;
+			st[i][j] = 9*9*i+1;
 			ed[i][j] = -1;
 		}
 	}
 
-	for(int j=0; j<9; j++){
+	for(int j=0; j<=9; j++){
 		g[1][j][j*j] = 1;
-		st[1][j] = j;
-		ed[1][j] = j;
+		st[1][j] = j*j;
+		ed[1][j] = j*j;
 	}
 
 	for(int i=2; i<19; i++){
 		for(int j=0; j<9*(i-1)+1; j++){
-			for(int k=st[i-][j]; k<=ed[i-1][j]; k++){
-				if(g[i-1][j][k] != 0)
-				
+			for(int k=st[i-1][j]; k<=ed[i-1][j]; k++){
+				if(g[i-1][j][k] != 0){
+					for(int l=0; l<=9; l++){
+						g[i][j+l][k+l*l] += g[i-1][j][k];
+						if(st[i][j+l] > k+l*l)
+							st[i][j+l] = k+l*l;
+						if(ed[i][j+l] < k+l*l)
+							ed[i][j+l] = k+l*l;
+					}
+				}
+			}
+		}
+	}
+}
+
+long long getH(long long ***g, long long ***h, int **st, int **ed, int i, int A, int B, int *primeMap){
+
+	//printf("st %d %d %d %d\n", i, A, B, h[i][A][B]);
+	if(h[i][A][B] > 0){
+		return h[i][A][B];
+	}
+
+	int aMax = 9*i;
+	int bMax = 81*i;
+	for(int aa=0; aa<=aMax; aa++){
+		if(primeMap[A+aa]){
+			for(int bb=st[i][aa]; bb<=bMax && bb<=ed[i][aa] && bb<=aa*aa; bb++){
+				//printf("%d %d %d:%d %d\n", i, aa, bb, st[i][aa], ed[i][aa]);
+				if(primeMap[B+bb]){
+					h[i][A][B] += g[i][aa][bb];
+				}
+			}
+		}
+	}
+
+	return h[i][A][B];
+}
+
+long long getNumber(string str, long long ***g, long long ***h, int **st, int **ed, int *primeMap){
+	long long result = 0L;
+	int N = str.length()-1;
+	int A=0, B=0;
+	for(int i=N; i>0; i--){
+		int num = int(str[N-i] - '0');
+		int t1 = i;
+		for(int j=0; j<num; j++){
+			result += getH(g, h, st, ed, t1, A+j, B+j*j, primeMap);
+		}
+		A += num;
+		B += num*num;
+	}
+	return result;
 }
 
 int main(){
@@ -61,7 +111,7 @@ int main(){
 	int *primeMap = (int*)malloc(1500*sizeof(int));
 	getPrimeMap(primeMap, 1500);
 
-	int ***g;
+	long long ***g, ***h;
 	int **st, **ed;
 	st = new int*[19];
 	ed = new int*[19];
@@ -70,31 +120,45 @@ int main(){
 		ed[i] = new int[9*i+1];
 	}
 
-	g = new int**[19];
-	for(int i=0;i<19; i++)
-		g[i] = new int*[9*i+1];
+	g = new long long**[19];
+	h = new long long**[19];
+	for(int i=0;i<19; i++){
+		g[i] = new long long*[9*i+1];
+		h[18-i] = new long long*[9*i+1];
+	}
 	for(int i=0;i<19; i++){
 		for(int j=0; j<9*i+1;j++){
-			g[i][j] = new int[81*i+1];
+			g[i][j] = new long long[81*i+1];
+			h[18-i][j] = new long long[81*i+1];
 		}
 	}
-	//computeG(g, st, ed);
+	computeG(g, h, st, ed);
 
-	
+	/*
+	for(int i=1; i<4; i++){
+		for(int j=0; j<9*i+1; j++){
+			printf("%d %d: %d %d\n", i, j, st[i][j], ed[i][j]);
+		}
+	}*/
+
 	string a, b;
 	char buf[100];
 	char *input = buf, *token;
+	long long resultA, resultB;
 		
 	while(T>0){
-		gets(buf);
+		fgets(buf, sizeof(buf), stdin);
 		token = strtok(input, " ");
 		a = token;
-		token = strtok(NULL, " ");
+		token = strtok(NULL, " \n");
 		b = token;
+		//printf("%s\n", a.c_str());
+		//printf("%s\n", b.c_str());
 
-		printf("%s\n", a.c_str());
-		printf("%s\n", b.c_str());
+		resultA = getNumber(a, g, h, st, ed, primeMap);
+		resultB = getNumber(b, g, h, st, ed, primeMap);
 
+		printf("%Ld\n", resultB-resultA);
 		T--;
 	}
 
@@ -108,10 +172,13 @@ int main(){
 	for(int i=0; i<19; i++){
 		for(int j=0; j<9*i+1; j++){
 			delete[] g[i][j];
+			delete[] h[18-i][j];
 		}
 	}
 	for(int i=0; i<19; i++){
 		delete[] g[i];
+		delete[] h[18-i];
 	}
 	delete[] g;
+	delete[] h;
 }
